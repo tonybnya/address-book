a3bc9d459e2d50aee738d8a8f0beca63ada443d4
Tony <nya.tony2010@gmail.com>
2024-02-20 20:51:25 +0100

    feat: add the configurations of the API

---
+++ backend/config.py
@@ -0,0 +1,15 @@
+"""
+This module contains the main configurations
+of the app (CRUD backend).
+"""
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)

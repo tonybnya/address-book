12f6ca34f64f92861bf13d8743a199d207d0f4fb
Tony <nya.tony2010@gmail.com>
2024-02-23 18:50:11 +0100

    style: add typing to config.py file

--- backend/config.py
+++ backend/config.py
@@ -6,10 +6,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
-app = Flask(__name__)
+app: Flask = Flask(__name__)
 CORS(app)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_DATABASE_URI"]: str = "sqlite:///database.db" # type: ignore
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]: bool = False # type: ignore
 
-db = SQLAlchemy(app)
+db: SQLAlchemy = SQLAlchemy(app)

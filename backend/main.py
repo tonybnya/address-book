5c3b6c699144ec166baee58f59390575fe47e46e
Tony <nya.tony2010@gmail.com>
2024-02-23 19:11:07 +0100

    feat: add context to the main module to avoid creating a new db if there is already an existing one

--- backend/main.py
+++ backend/main.py
@@ -2,3 +2,13 @@
 This module contains the main routes/endpoints
 of the API (CRUD backend).
 """
+from config import app, db
+from flask import jsonify, request
+from models import Contact
+
+
+if __name__ == "__main__":
+  with app.app_context():
+    db.create_all()
+
+  app.run(host="0.0.0.0", debug=True)

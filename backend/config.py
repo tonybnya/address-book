c4962dc67ded31b19be9297400de2b636910144f
Tony <nya.tony2010@gmail.com>
2024-02-23 19:09:41 +0100

    style: add typing to config.py file

--- backend/config.py
+++ backend/config.py
@@ -3,8 +3,8 @@ This module contains the main configurations
 of the app (CRUD backend).
 """
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
 
 app: Flask = Flask(__name__)
 CORS(app)

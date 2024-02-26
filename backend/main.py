ab371ddae4ea723fe7c4c1de1e2c5d421a427cac
Tony <nya.tony2010@gmail.com>
2024-02-26 10:24:13 +0100

    feat: create an endpoint to create a new contact with the required details

--- backend/main.py
+++ backend/main.py
@@ -6,26 +6,35 @@ from config import app, db
 from flask import jsonify, request
 from models import Contact
 
-# @app.route('/create_contact', methods=['POST'])
-# def create_contact():
-#   first_name = request.json.get('firstName')
-#   last_name = request.json.get('lastName')
-#   email = request.json.get('email')
-#   role = request.json.get('role')
-#   phone_number = request.json.get('phoneNumber')
-#   category = request.json.get('category')
-#   bio = request.json.get('category')
-#
-#   if not first_name or not last_name or not email or not role or not phone_number or not category or not bio:
-#     return jsonify({"message": "You must enter all the fiels"}), 400
-#
-#   new_contact = Contact(first_name=first_name, last_name=last_name, email=email, role=role, phone_number=phone_number, category=category, bio=bio)
-#
-#   try:
-#     db.session.add(new_contact)
-#     db.session.commit()
-#   except Exception as e:
-#     return jsonify({"message", str(e)}), 400
+@app.route('/create_contact', methods=['POST'])
+def create_contact():
+    """
+    Endpoint to create a new contact by providing the required details
+
+    :return json: a JSON response with a message indicating the success or the failure of the operation
+    """
+    # extract required details from the request body
+    first_name = request.json.get('firstName')
+    last_name = request.json.get('lastName')
+    email = request.json.get('email')
+    role = request.json.get('role')
+    phone_number = request.json.get('phoneNumber')
+    category = request.json.get('category')
+    bio = request.json.get('category')
+
+    # check if all the required fields are provided
+    if not first_name or not last_name or not email or not role or not phone_number or not category or not bio:
+        return jsonify({"message": "You must enter all the fiels"}), 400
+
+    # create a new contact object
+    new_contact = Contact(first_name=first_name, last_name=last_name, email=email, role=role, phone_number=phone_number, category=category, bio=bio)
+
+    try:
+        # add the new contact to the database
+        db.session.add(new_contact)
+        db.session.commit()
+    except Exception as e:
+        return jsonify({"message", str(e)}), 400
 
 
 @app.route('/contacts', methods=['GET'])

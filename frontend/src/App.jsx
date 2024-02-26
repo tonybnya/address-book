b372a2eb6bc6a2e8b10d14409b5a70108a4d2ac5
Tony <nya.tony2010@gmail.com>
2024-02-26 14:24:03 +0100

    restart the frontend directory

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,18 @@
-import { useState, useEffect } from 'react';
-import './App.css';
+import { useState, useEffect } from 'react'
+import './App.css'
 
 function App() {
   const [contacts, setContacts] = useState([]);
 
   useEffect(() => {
-    fetchContacts();
-  });
-
+    fetchContacts()
+  }, []);
   const fetchContacts = async () => {
-    const log = (arg) => console.log(arg);
-    const response = await fetch("http://127.0.0.1:5555/contacts");
+    const response = await fetch("http://127.0.0.1:5000/contacts");
     const data = await response.json();
     setContacts(data.contacts);
-    log(data.contacts);
   };
+
   return (
     <>
     </>

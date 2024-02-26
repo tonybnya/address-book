146dedda97d03c8d538732ba69c29687d2bf39d2
Tony <nya.tony2010@gmail.com>
2024-02-26 09:45:44 +0100

    docs: document the config.py file

--- backend/config.py
+++ backend/config.py
@@ -6,10 +6,15 @@ from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
+# create an instance of a Flask application
 app: Flask = Flask(__name__)
+# enable Cross-Origin Resource Sharing (CORS)
 CORS(app)
 
+# configure the SQLAlchemy database URI
 app.config["SQLALCHEMY_DATABASE_URI"]: str = "sqlite:///database.db" # type: ignore
+# disable modifications tracking for SQLAlchemy to suppress warning
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]: bool = False # type: ignore
 
+# initialize an instance of the SQLAlchemy database 
 db: SQLAlchemy = SQLAlchemy(app)

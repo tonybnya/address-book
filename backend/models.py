27835d33cdd8abb0960cc68d78fbbdb0f4cb29d4
Tony <nya.tony2010@gmail.com>
2024-02-26 09:58:16 +0100

    docs: add docstrings to models.py file

--- backend/models.py
+++ backend/models.py
@@ -7,23 +7,41 @@ from typing import Dict
 
 
 class Contact(db.Model):
-  id: int = db.Column(db.Integer, primary_key=True)
-  first_name: str = db.Column(db.String(80), unique=False, nullable=False)
-  last_name: str = db.Column(db.String(80), unique=False, nullable=False)
-  email: str = db.Column(db.String(120), unique=True, nullable=False)
-  role: str = db.Column(db.String(80), unique=False, nullable=False)
-  phone_number: str = db.Column(db.String(80), unique=False, nullable=False)
-  category: str = db.Column(db.String(80), unique=False, nullable=False)
-  bio: str = db.Column(db.String(500), unique=False, nullable=False)
+    """
+    Contact class representing the model of a contact in the database
 
-  def to_json(self) -> Dict[str, str]:
-    return {
-      "id": self.id,
-      "firstName": self.first_name,
-      "lastName": self.last_name,
-      "email": self.email,
-      "role": self.role,
-      "phoneNumber": self.phone_number,
-      "category": self.category,
-      "bio": self.bio
-    }
+    Attributes:
+        id (int): unique identifier of the contact
+        first_name (str): first name of the contact
+        last_name (str): last name of the contact
+        email (str): email of the contact
+        role (str): role of the contact
+        phone_number (str): phone number of the contact
+        category (str): category of the contact
+        bio (str): short biography/description of the contact
+    """
+    id: int = db.Column(db.Integer, primary_key=True)
+    first_name: str = db.Column(db.String(80), unique=False, nullable=False)
+    last_name: str = db.Column(db.String(80), unique=False, nullable=False)
+    email: str = db.Column(db.String(120), unique=True, nullable=False)
+    role: str = db.Column(db.String(80), unique=False, nullable=False)
+    phone_number: str = db.Column(db.String(80), unique=False, nullable=False)
+    category: str = db.Column(db.String(80), unique=False, nullable=False)
+    bio: str = db.Column(db.String(500), unique=False, nullable=False)
+
+    def to_json(self) -> Dict[str, str]:
+        """
+        Convert the Contact object to a JSON-compatible dictionary
+
+        :return dict: a dictionary representing the Contact object in JSON format
+        """
+        return {
+            "id": self.id,
+            "firstName": self.first_name,
+            "lastName": self.last_name,
+            "email": self.email,
+            "role": self.role,
+            "phoneNumber": self.phone_number,
+            "category": self.category,
+            "bio": self.bio
+        }
